4927d4b8be400ea6f6e803c378041f861aaf9cb5
Suprgya Bhushan <suprgya.56@iiitb.org>
2018-11-28 02:49:10 +0530

    hello

--- voicexblock/static/js/src/voicexblock.js
+++ voicexblock/static/js/src/voicexblock.js
@@ -1,61 +1,7 @@
   // Now we add the most important point of the plugin, the commands
   // This library is very flexible and now we will see why :
   // Every command is a literal object
-  /*artyom.addCommands([
-    {
-      description:"Artyom can talk too, lets say something if we say hello",
-      indexes:["hello","hey"],
-      action:function(i){
-          // i = the index of the array of indexes option
 
-          if(i == 0){
-            //You say : "hello"
-            document.getElementById('time').innerHTML = "Hello ! How are you? I don't want to talk today";
-          }
-      }
-    },
-    {
-      description:"Say goodbye",
-      indexes:["goodbye"],
-      action:function(){
-        alert("Now all is over.");
-      }
-    },
-    {
-      description:"Say what time is it",
-      indexes:['what time is it'],
-      action:function(){
-       var currentdate = new Date();
-       var datetime = "About the date : " + currentdate.getDate() + "/"
-            + (currentdate.getMonth()+1)  + "/"
-            + currentdate.getFullYear() + " @ "
-            + currentdate.getHours() + ":"
-            + currentdate.getMinutes() + ":"
-            + currentdate.getSeconds();
-        document.getElementById('time').innerHTML = datetime;
-      }
-    },
-    {
-      description: "Smart command, say how much x in what we say",
-      indexes:["what's the number of *"],
-      smart:true,
-      action:function(i,wildcard){
-        document.getElementById('time').innerHTML = "The number of " + wildcard + ' is '+ Math.floor(Math.random() * 4000) + 1;
-      }
-    }
-  ]);
-
-
-  // Redirect the recognized text
-  artyom.redirectRecognizedTextOutput(function(text,isFinal){
-    var span = document.getElementById('output');
-
-    if(isFinal){
-      span.innerHTML = '';
-    }else {
-      span.innerHTML = text;
-    }
-  });*/
 
   function startArtyom(){
     artyom.initialize({
@@ -72,6 +18,59 @@
 
   window.onload = function(){
       var tab = document.getElementById("commands-list");
+      artyom.addCommands([
+        {
+          description:"Artyom can talk too, lets say something if we say hello",
+          indexes:["hello","hey"],
+          action:function(i){
+              // i = the index of the array of indexes option
+
+              if(i == 0){
+                //You say : "hello"
+                document.getElementById('time').innerHTML = "Hello ! How are you? I don't want to talk today";
+              }
+          }
+        },
+        {
+          description:"Say goodbye",
+          indexes:["goodbye"],
+          action:function(){
+            alert("Now all is over.");
+          }
+        },
+        {
+          description:"Say what time is it",
+          indexes:['what time is it'],
+          action:function(){
+           var currentdate = new Date();
+           var datetime = "About the date : " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+            document.getElementById('time').innerHTML = datetime;
+          }
+        },
+        {
+          description: "Smart command, say how much x in what we say",
+          indexes:["what's the number of *"],
+          smart:true,
+          action:function(i,wildcard){
+            document.getElementById('time').innerHTML = "The number of " + wildcard + ' is '+ Math.floor(Math.random() * 4000) + 1;
+          }
+        }
+      ]);
+      // Redirect the recognized text
+      artyom.redirectRecognizedTextOutput(function(text,isFinal){
+        var span = document.getElementById('output');
+
+        if(isFinal){
+          span.innerHTML = '';
+        }else {
+          span.innerHTML = text;
+        }
+      });
       var commands =  artyom.getAvailableCommands();
       var html = '';
 
