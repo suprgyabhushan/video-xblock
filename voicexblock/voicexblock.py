d5f199d35b808eab8ddc2100d84f71282dab6041
Suprgya Bhushan <suprgya.56@iiitb.org>
2018-11-28 01:55:47 +0530

    work

--- voicexblock/voicexblock.py
+++ voicexblock/voicexblock.py
@@ -11,7 +11,7 @@ class VoiceXBlock(StudioEditableXBlockMixin, XBlock):
 
     # Fields are defined on the class.  You can access them in your code as
     # self.<fieldname>.
-    display_name = String(display_name="Display name", default='Video', scope=Scope.settings)
+    display_name = String(display_name="Display name", default='Voice', scope=Scope.settings)
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -20,13 +20,13 @@ class VoiceXBlock(StudioEditableXBlockMixin, XBlock):
 
     def student_view(self, context=None):
         """
-        The primary view of the VideoXBlock, shown to students
+        The primary view of the VoiceXBlock, shown to students
         when viewing courses.
         """
         html = self.resource_string("static/html/voicexblock.html")
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/voicexblock.css"))
-        frag.add_javascript(self.resource_string("static/js/src/voicexblock.js"))
+        # frag.add_javascript(self.resource_string("static/js/src/voicexblock.js"))
 
         return frag
 
