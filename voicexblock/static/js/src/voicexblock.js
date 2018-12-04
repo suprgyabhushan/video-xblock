4cd6fb40c48ba4a4c0bdca2c0ec8d335077ecfe9
Suprgya Bhushan <suprgya.56@iiitb.org>
2018-12-04 12:22:52 +0530

    n

--- voicexblock/static/js/src/voicexblock.js
+++ voicexblock/static/js/src/voicexblock.js
@@ -39,7 +39,7 @@ window.onkeypress = onPagePress;
 function onPagePress(e) {
   var charCode = (typeof e.which == "number") ? e.which : e.keyCode
   if (charCode == 113) {
-    window.getElementById('redirect_link').click();
+    document.getElementById('redirect_link').click();
     //window.location = "http://www.google.com/";
 
  }
